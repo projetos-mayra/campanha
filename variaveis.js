1339145cfe1d0edc965bfe5c9042616eae515096
Mayra Bruno <80061821+projetos-mayra@users.noreply.github.com>
2021-11-30 09:24:19 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,19 @@
+function pedirValores(){
+    s1 = (prompt ("Digite a Campanha: "));
+    s2 = (prompt ("Informe seu nome:"));
+    s3 = parseInt (prompt ("Qual o Valor da sua compra: "));
+    //x = parseInt (prompt ("Digite um Valor para x:"));
+    //y = parseInt (prompt ("Dígite um valor para y:"));
+    //z = parseInt (prompt ("Digite um valor para z"));
+    pontos = (s3*2);
+
+
+//s=s1+" "+s2+" "+s3;         // concatenação de strings
+//soma=x+y;                   // adição
+//mult=x*y;                   // multiplicação
+//div=x/y;                    // divisão
+//sub=x-y;                    // subtracção
+//mod=x%y;                  // resto da divisão inteira
+
+
+}
